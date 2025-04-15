c87de741105b8f6a1fd8da02907c2fffdbff832c
atillakhalilli0 <atilla.xalilli@gmail.com>
2025-04-15 19:44:55 +0400

    com4

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/package.json', (req, res) => {
-  const data = JSON.parse(fs.readFileSync('./package.json'));
+app.get('/data.json', (req, res) => {
+  const data = JSON.parse(fs.readFileSync('./data.json'));
   res.json(data);
 });
 
-app.get('/package.json/:category', (req, res) => {
-  const data = JSON.parse(fs.readFileSync('./package.json'));
+app.get('/data.json/:category', (req, res) => {
+  const data = JSON.parse(fs.readFileSync('./data.json'));
   const category = req.params.category;
   
   if (data[category]) {
